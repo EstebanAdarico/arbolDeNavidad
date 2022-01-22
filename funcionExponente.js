4564843da0ca5263d95bf0430b3d4439bbb13970
bann_perseverance <estebangpa41@gmail.com>
2022-01-22 12:40:40 -0500

    resolucionDelLibroEloquent

---
+++ funcionExponente.js
@@ -0,0 +1,10 @@
+
+const potencia = function (base, exponente){
+   for (let iterations = 0; iterations < exponente; iterations++) {
+      return base = base * iterations
+      
+   }
+} 
+console.log(potencia(2 ,10)) 
+
+   
---
+++ tableroDeAjedrez.js
@@ -0,0 +1,7 @@
+let valorAntes = " #"
+let valorDespues = "# "
+for (let i = 0; i < 4; i++) {
+   console.log(`${valorAntes.repeat(4)}`)
+   console.log(`${valorDespues.repeat(4)}`)
+   
+}
